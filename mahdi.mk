45aea519a5d6a6396d23b483ceb60519887a5397
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-07-16 18:59:27 +0100

    cm: Update fingerprint/desc to latest
    Change-Id: I81d2ce2872ed7534e2e1723f61c070b32575d4c8
    
    Conflicts:
    	mahdi.mk

--- mahdi.mk
+++ mahdi.mk
@@ -40,4 +40,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=A0001 PRODUCT_NAME=bacon
 TARGET_CONTINUOUS_SPLASH_ENABLED := true
 
 ## Use the latest approved GMS identifiers unless running a signed build
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=oneplus/bacon/A0001:4.4.2/KVT49L/XNPH22R:user/release-keys PRIVATE_BUILD_DESC="bacon-user 4.4.2 KVT49L XNPH22R release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=oneplus/bacon/A0001:4.4.2/KVT49L/XNPH25R:user/release-keys PRIVATE_BUILD_DESC="bacon-user 4.4.2 KVT49L XNPH25R release-keys"
+
